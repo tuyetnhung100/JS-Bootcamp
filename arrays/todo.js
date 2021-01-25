90de57f301e78e05fd3d85c15d51980aeab468e8
Nina <tuyetnhung100@me.com>
2021-01-25 06:35:58 +0300

    Filtering Arrays

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,33 +1,39 @@
-const todos = [ {
-    text: 'Eat breakfast',
+const todos = [{
+    text: 'Exercise',
+    completed: false 
+}, {
+    text: 'Clean kitchen',
     completed: true
 }, {
     text: 'Feed the cat',
-    completed: false
-}, {
-    text: 'Exercise',
     completed: true
 }, {
     text: 'Buy food',
-    completed: true
+    completed: false    
 }, {
-    text: 'Do laundry',
-    completed: false
-} ]
-
+    text: 'Do work',
+    completed: true    
+}]
 // 1. Convert array to array of objects -> text, completed
 // 2. Create function to remove a todo by text value
 
-const deleteTodo = function (myTodos, todosText) {
-    const todosIndex = todos.findIndex( function (todo) {
+const deleteTodo = function (todos, todosText) {
+    const index = todos.findIndex(function (todo) {
         return todo.text.toLowerCase() === todosText.toLowerCase()
     })
 
-    if (todosIndex > -1) {
-        todos.splice(todosIndex, 1)
+    if(index > -1) {    // if the return index is greater than -1, we know we have a match
+        todos.splice(index, 1)
     }
 }
 
-deleteTodo(todos, 'buy food')
-console.log(todos)
+const getThingsToDo = function (todos) {
+    return todos.filter(function (myTodo, index) {
+        return !myTodo.completed    // same as myTodo.completed === false
+    })
+}
+
+console.log(getThingsToDo(todos))
+// deleteTodo(todos, 'buy food')
+// console.log(todos)
 
