b61092fac5efa556801d373e264a0990be85ab18
Nina <tuyetnhung100@me.com>
2021-01-27 17:35:44 +0300

    Calculate total income, total expenses, and account balance.

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,6 +1,7 @@
 const account = {
     name: 'Nina Hoang',
     expenses: [],
+    income: [],
     addExpense: function (description, amount) {
         const item = {
             description: description, 
@@ -8,13 +9,29 @@ const account = {
         }
         this.expenses.push(item)
     },
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
     // total up all expenses
     getAccountSummary: function () {
-        let sum = 0
+        let totalExpense = 0
+        let totalIncome = 0
+        let accountBalance = 0
+
         this.expenses.forEach(function (element) {
-            sum += element.amount
+            totalExpense += element.amount
         })
-        return `Nina Hoang has $${sum} (${sum * 75}rub.) in expenses`
+
+        this.income.forEach(function (element) {
+            totalIncome += element.amount
+        })
+
+        accountBalance = totalIncome - totalExpense
+        
+        return `${account.name} has a balance of $${accountBalance}. $${totalIncome} (${totalIncome * 75}rub.) in income. $${totalExpense} (${totalExpense * 75}rub.) in expenses.`
     },
     sortExpense: function (expenses) {
         this.expenses.sort((a, b) => b.amount - a.amount) // descending order (largest to smallest sort)
@@ -30,5 +47,6 @@ account.addExpense('Pet', 5)
 account.addExpense('Shopping', 74)
 account.sortExpense(account.expenses)
 console.log(account.expenses)
+account.addIncome('Job', 470)
 console.log('---------------------------------------------')
 console.log(account.getAccountSummary())
