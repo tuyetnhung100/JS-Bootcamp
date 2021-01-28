5eaaef88a9b212d791274422fa18ba3b949b86a0
Nina <tuyetnhung100@me.com>
2021-01-28 04:14:11 +0300

    Adding Elemens via the DOM

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,33 @@
-const paragraphs = document.querySelectorAll('p')
+const todos = [{
+    text: 'Exercise',
+    completed: false 
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Feed the cat',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: false    
+}, {
+    text: 'Do work',
+    completed: true    
+}]
 
-paragraphs.forEach(function (paragraph) {
-    if (paragraph.textContent.includes('the')) {
-        paragraph.remove()
-    }
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
 })
+
+// You have 2 todos left (p element)
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+// Add a p for each todo above (use text value)
+todos.forEach(function (todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})
+
